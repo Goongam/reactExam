967452439fdffac45fc13a5f77ef8fbce6083bce
Goongam <dlwjddn341@naver.com>
2022-07-05 20:48:54 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [data, setData] = useState(["남자 코트 추천","강남 고기 맛집","해외 여행 추천"]);
+  const aa = [1,2];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        개발 Blog
+      </div>
+      
+      {
+        data.map((data,index)=>{
+          return (
+            <div className='list' key={index}>
+              <h3> {data} </h3>
+              <p>2월 17일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
+
     </div>
   );
 }
