839be70c8595c3286ef11256a5a9c2e4df35874d
Goongam <dlwjddn341@naver.com>
2022-07-08 21:28:54 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,28 +7,53 @@ import { useState } from 'react';
 function App() {
   
   const [data, setData] = useState(["남자 코트 추천","강남 고기 맛집","해외 여행 추천"]);
-  const aa = [1,2];
+  const [따봉, 따봉변경] = useState(0);
+
+  // function changeData(){
+  //   let newArray = [...data];
+  //   //newArray[0] = '여자 코트 추천';
+  //   newArray.sort();
+  //   setData(newArray);
+  // }
 
   return (
     <div className="App">
       <div className='black-nav'>
         개발 Blog
       </div>
+      {/* <button onClick={changeData}>aaaa</button> */}
+      <div className='list'>
+        <h3>{data[0]} <span onClick={ ()=>{따봉변경( 따봉+1 )} }> 👍 </span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+      </div>
+
+      <div className='list'>
+        <h3>{data[1]} <span></span></h3>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+      </div>
+
+      <div className='list'>
+        <h3>{data[2]} <span></span></h3>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+      </div>
+
       
-      {
-        data.map((data,index)=>{
-          return (
-            <div className='list' key={index}>
-              <h3> {data} </h3>
-              <p>2월 17일 발행</p>
-              <hr/>
-            </div>
-          )
-        })
-      }
-
-
+      <Modal />
+      
     </div>
+  );
+}
+
+function Modal(){
+  return(
+      <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
   );
 }
 
