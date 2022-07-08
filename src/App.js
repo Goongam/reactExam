6c248e24b77cba5c6e739f5d2d75cbe8d8cb0a69
Goongam <dlwjddn341@naver.com>
2022-07-09 05:14:36 +0900

    day3

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@
 
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useMemo, useState, memo, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 function App() {
   
   const [data, setData] = useState(["남자 코트 추천","강남 고기 맛집","해외 여행 추천"]);
   const [따봉, 따봉변경] = useState(0);
 
-  // function changeData(){
-  //   let newArray = [...data];
-  //   //newArray[0] = '여자 코트 추천';
-  //   newArray.sort();
-  //   setData(newArray);
-  // }
+  const [title, setTitle] = useState("제목1");
+  const changeTitle = ()=>{
+    setTitle("바뀐제목");
+  }
+
+
+
+  useEffect(()=>{console.log("함수호출 title:",title);},[title]);
 
   return (
     <div className="App">
@@ -28,31 +31,26 @@ function App() {
         <hr></hr>
       </div>
 
-      <div className='list'>
-        <h3>{data[1]} <span></span></h3>
-        <p>2월 17일 발행</p>
-        <hr></hr>
-      </div>
-
-      <div className='list'>
-        <h3>{data[2]} <span></span></h3>
-        <p>2월 17일 발행</p>
-        <hr></hr>
-      </div>
-
       
-      <Modal />
+      <Modal title={title} date="날짜" onClick={changeTitle} />
+      <Modal title="제목" />
       
     </div>
   );
 }
 
-function Modal(){
+Modal.propTypes = {
+  onClick: PropTypes.func,
+}
+
+// const MemorizedCompo = memo(Modal);
+function Modal({title, date, onClick}){
   return(
       <div className='modal'>
-        <h2>제목</h2>
-        <p>날짜</p>
+        <h2>{title}</h2>
+        <p>{date}</p>
         <p>상세내용</p>
+        <button onClick={onClick}>click!</button>
       </div>
   );
 }
