280e07f9555752966cbe6c3eee51c540ad876fdd
Goongam <dlwjddn341@naver.com>
2022-07-12 00:56:21 +0900

    day 5

--- src/Component/CoinTracker.js
+++ src/Component/CoinTracker.js
@@ -1,9 +1,13 @@
-import { useMemo, useState, memo, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function CoinTracker(){
 //https://api.coinpaprika.com/v1/tickers
     const [data, setData] = useState([]);
     const [sortPriceASC, setSortPriceASC] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [searchCoin, setSearchCoin] = useState('');
+
+    const [Coins, setCoins] = useState([]);
 
     useEffect(()=>{
         setTimeout( ()=>{
@@ -11,7 +15,9 @@ function CoinTracker(){
                 (response)=>response.json()
             ).then(
                 (jsonData)=>{
+                    setCoins(jsonData);
                     setData(jsonData);
+                    setLoading(false);
                 }
             )
         }, 0 );
@@ -37,31 +43,56 @@ function CoinTracker(){
         setData(copyData);
         setSortPriceASC(true);
     }
+
+    function changeSearch(e){
+        setSearchCoin(e.target.value);
+    }
+    function keyDownSearch(e){
+        if(e.key === "Enter") SearchCoin()        
+    }
+
+    function SearchCoin(){
+        let CopyData = Coins;
+        CopyData = CopyData.filter((value)=> value.name.includes(searchCoin) );
+        
+        setData(CopyData);
+        console.log(CopyData.length);
+    }
+
     return (
         <div>
-            
             <h2>Coins ({data.length}) </h2>
-            <table style={ {"textAlign":"center"} }>
-                <tr>
-                    <th>name</th>
-                    <th>symbol</th>
-                    <th>price {sortPriceASC ? 
-                        <button onClick={coinSortByPriceASC}>▼</button> : 
-                        <button onClick={coinSortByPriceDESC}>▲</button>}  
-                    </th>
-                </tr>
+            
+            {loading ? 'loading...':
+                <div>
+                    <input type='text' placeholder='Search CoinName...' onKeyDown={keyDownSearch}  onChange={changeSearch} value={searchCoin} ></input>
+                    <button onClick={SearchCoin}>SEARCH</button>
+                    { data.length === 0 ? <div>No Result</div>:
+                        <table style={ {"textAlign":"center"} }>
+                            
+                                <tr>
+                                    <th>name</th>
+                                    <th>symbol</th>
+                                    <th>price {sortPriceASC ? 
+                                        <button onClick={coinSortByPriceASC}>▼</button> : 
+                                        <button onClick={coinSortByPriceDESC}>▲</button>}  
+                                    </th>
+                                </tr>
+                            
 
-            {
-                data.map( (coin,index)=>
-                    <tr key={index}>
-                        <td>{coin.name}</td>
-                        <td>{coin.symbol}</td>
-                        <td>{coin.quotes.USD.price}</td>
-                    </tr>
-             )
+                            {
+                                data.map( (coin,index)=>
+                                    <tr key={index}>
+                                        <td>{coin.name}</td>
+                                        <td>{coin.symbol}</td>
+                                        <td>{coin.quotes.USD.price}</td>
+                                    </tr>
+                                )
+                            }
+                        </table>
+                    }
+                </div>
             }
- 
-            </table>
         </div>
     );
 }
